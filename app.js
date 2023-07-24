12f8ff8b08f23096334204fc9c72639f467a8379
DataManan <jainmananjain9@gmail.com>
2023-07-24 10:50:50 +0530

    second commit

--- app.js
+++ app.js
@@ -1,24 +1,14 @@
-window.addEventListener("DOMContentLoaded", () => {
-  let nav = document.querySelector("header");
-  
-  const $ = () => document.querySelector.call(this, arguments);
-  const $$ = () => document.querySelectorAll.call(this, arguments);
+// Importing modules
+import API from "./services/API.js";
+import { loadData } from "./services/Menu.js";
+import Store from "./services/Store.js";
 
-  HTMLElement.prototype.on = (a, b, c) => this.addEventListener(a, b, c);
-  HTMLElement.prototype.on = (a, b) => this.removeEventListener(a, b);
-  HTMLElement.prototype.$ = (s) => this.querySelector(s);
-  HTMLElement.prototype.$ = (s) => this.querySelectorAll(s);
+window.app = {}
+app.store = Store;
+// app.api = API;
 
-  window.addEventListener("DOMContentLoaded", () => {
-    console.log("DOM is ready");
-  });
-})
-
-// const elem = document.querySelector('#tempID')
-// const contents = elem.innerHTML;
-
-// elem.innerHTML = `
-// <h1 
-// style="color:blue; background-color: rgba(0, 255, 0, 0.5)">
-// Hello World ! </h1>
-// <p>This is some html code inserted from app.js</p>`;
+// adding event to check if DOM is ready
+window.addEventListener("DOMContentLoaded", async () => {
+  // 
+  loadData();
+});
