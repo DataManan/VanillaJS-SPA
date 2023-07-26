44df415749d313c0355693aa8fa8a2943bf1eaec
DataManan <jainmananjain9@gmail.com>
2023-07-26 23:55:57 +0530

    proxies for custom components

--- components/MenuPage.js
+++ components/MenuPage.js
@@ -41,6 +41,13 @@ export class MenuPage extends HTMLElement {
                 </ul>
                 `;
                 this.root.querySelector("#menu").appendChild(liCategory);
+
+                category.products.forEach(product => {
+                    const item = document.createElement("product-item");
+                    item.dataset.product = JSON.stringify(product);
+                    liCategory.querySelector("ul").appendChild(item);
+
+                });
             }
         } else {
             this.root.querySelector("#menu").innerHTML = "Loading...";
