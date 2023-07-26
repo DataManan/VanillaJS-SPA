25311ffe36978cdb5a567820418b76a70a046ce3
DataManan <jainmananjain9@gmail.com>
2023-07-26 23:23:30 +0530

    proxies for custom components

--- components/MenuPage.js
+++ components/MenuPage.js
@@ -8,16 +8,17 @@ export class MenuPage extends HTMLElement {
         // this.root.appendChild(content); 
 
         const styles = document.createElement("style");
-        this.root.appendChild(styles);
+        
         // ccreating a function to load css
         async function loadCSS() {
-            const request = await fetch("/compoments/MenuPage.css");
+            const request = await fetch("./components/MenuPage.css");
             const css = await request.text();
             styles.textContent = css;
         }
 
         // calling loadCSS function
         loadCSS();
+        this.root.appendChild(styles);
     }
 
     // when component is connected to DOM
@@ -25,6 +26,25 @@ export class MenuPage extends HTMLElement {
         const template = document.getElementById("menu-page-template");
         const content = template.content.cloneNode(true);
         this.root.appendChild(content);
+
+        window.addEventListener("appmenuchange", () => {
+            this.render();
+        })
+    }
+    render() {
+        if (app.store.menu) {
+            for (let category of app.store.menu) {
+                const liCategory = document.createElement("li")
+                liCategory.innerHTML = `
+                <h3>${category.name}</h3>
+                <ul className="category">
+                </ul>
+                `;
+                this.root.querySelector("#menu").appendChild(liCategory);
+            }
+        } else {
+            this.root.querySelector("#menu").innerHTML = "Loading...";
+        }
     }
 }
 
--- services/Store.js
+++ services/Store.js
@@ -3,4 +3,24 @@ const Store = {
     cart : []
 }
 
-export default Store;
+const ProxiedStore = new Proxy(Store, {
+    set(target, property, value) {
+        target[property] = value;
+        if (property == "menu") {
+          window.dispatchEvent(new Event("appmenuchange"));
+
+          /*
+            * why window and not document
+            * Because each component can has its own document aka shadow doms but,
+            * we would only have one window for our whole app!
+            * 
+            */
+        }
+        if (property == "cart") {
+            window.dispatchEvent(new Event("appcartchange"));
+        }
+        return true;
+    }
+})
+
+export default ProxiedStore;
